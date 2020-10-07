250b303e21281b790f69fb385b9c812064b546eb
Matthias Hölzl <tc@xantira.com>
2020-10-07 13:39:13 +0200

    Update Notebooks

--- src/simple-flask-app/app-livecoding.py
+++ src/simple-flask-app/app-livecoding.py
@@ -1,19 +1,19 @@
 # %%
 import io
 import json
-
-from torchvision import models
-import torchvision.transforms as transforms
-from PIL import Image
+from os import O_TRUNC
 from flask import Flask, jsonify, request
 
-# %%
+import torchvision.transforms as transforms
+from torchvision import models
+
+from PIL import Image
+
 app = Flask(__name__)
 imagenet_class_index = json.load(open('imagenet-class-index.json'))
 model = models.densenet121(pretrained=True)
 model.eval()
 
-# %%
 def transform_image(image_bytes):
     my_transforms = transforms.Compose([transforms.Resize(255),
                                         transforms.CenterCrop(224),
@@ -24,25 +24,27 @@ def transform_image(image_bytes):
     image = Image.open(io.BytesIO(image_bytes))
     return my_transforms(image).unsqueeze(0)
 
-
-# %%
 def get_prediction(image_bytes):
-    tensor = transform_image(image_bytes=image_bytes)
+    tensor = transform_image(image_bytes)
     outputs = model.forward(tensor)
-    _, y_hat = outputs.max(1)
+    value, y_hat = outputs.max(1)
     predicted_idx = str(y_hat.item())
-    return imagenet_class_index[predicted_idx]
+    return y_hat, imagenet_class_index[predicted_idx]
 
-
-# %%
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
+    print(f"Request: {request}, files: {dir(request)}")
+    if request.files:
         file = request.files['file']
         img_bytes = file.read()
-        class_id, class_name = get_prediction(image_bytes=img_bytes)
+        class_id, class_name = get_prediction(img_bytes)
         return jsonify({'class_id': class_id, 'class_name': class_name})
+    return "No files?"
 
-# %%
+@app.route("/hello")
+def hello():
+    return "Hello, world!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
+
---
+++ src/simple-flask-app/interactive.py
@@ -0,0 +1,12 @@
+# %%
+1 + 1
+
+# %%
+class MyClass:
+    def __init__(self) -> None:
+        print("Hallo!")
+
+# %%
+MyClass()
+
+# %%
